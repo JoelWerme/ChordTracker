a14408c75b6aa8f468d960d411c880c4bd05517f
Joel Werme <joelwer3@gmail.com>
2021-04-30 13:31:59 +0200

    minor fixes

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ document.querySelector("form").addEventListener("submit", (event) => {
     let inputData = document.querySelector("[data-input]").value;
     fetch("https://api.uberchord.com/v1/chords?names=" + inputData)
       .then((response) => response.json())
-      .then((data) => console.log(data))});
+      .then((data) => {
+        console.log(data)
+
+        let sprite = document.createElement('p');
+        sprite.setAttribute("src", data.sprites.front_default)
+        document.querySelector("#chords").prepend(sprite);
+        document.querySelector("[data-input]").value = "";
+        document.querySelector(
+          ".message"
+        ).innerText = `Chord:${inputData}`;
+      })
+    });
