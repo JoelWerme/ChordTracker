2fdc9f7f1d36df62e3f823ff36cc15969e257130
Joel Wermé <joelwer3@gmail.com>
2021-05-07 15:25:00 +0200

    minor changes to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let char = document.querySelector("#char")
+let appRoot = document.querySelector("#container")
 
 getData()
 
@@ -10,7 +10,15 @@ for(let i = 1; i <= 9; i++){
       console.log(data)
       for(let i = 0; i < data.results.length; i++){
         console.log(data.results[i].name)
-        char.append(printChar(data.results[i].name, data.results[i].films))
+        console.log(data.results[i].films)
+        generateChar(data.results[i].name)
+        // char.append(printChar(data.results[i].name, data.results[i].films,
+        //   fetch(data.results[i].films)
+        //     .then((response) => response.json())
+        //     .then((data) => {console.log(data) 
+        //       char.append(printChar(data.title))
+        //     }))
+        // )
       }
     })
     .catch(err =>{
@@ -19,14 +27,21 @@ for(let i = 1; i <= 9; i++){
   }
 }
 
-function printChar(name, films){
-  let charContainer = document.createElement("div")
-  charContainer.classList.add("char-container")
-  let charName = document.createElement("div")
-  let charFilms = document.createElement("div")
-  charFilms.innerText = films
-  charName.innerText = name
-  charContainer.append(charName, charFilms)
-
-  return charContainer
+function generateChar(name){
+  let charHeader = document.createElement("h2")
+  charHeader.innerText = name
+  appRoot.append(charHeader)
 }
+
+// function printChar(name, films, title){
+//   let charContainer = document.createElement("div")
+//   charContainer.classList.add("char-container")
+//   let charName = document.createElement("div")
+//   let charFilms = document.createElement("div")
+//   let filmsTitle = document.createElement("div")
+//   filmsTitle.innerText = title
+//   charFilms.innerText = films
+//   charName.innerText = name
+//   charContainer.append(charName, charFilms, filmsTitle)
+//   return charContainer
+// }
