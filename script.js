754d6161a88e7b993f3ca1ade4fcae4dc5836f77
Joel Wermé <joelwer3@gmail.com>
2021-05-07 19:56:21 +0200

    fixed character container

--- script.js
+++ script.js
@@ -11,14 +11,7 @@ for(let i = 1; i <= 9; i++){
       for(let i = 0; i < data.results.length; i++){
         console.log(data.results[i].name)
         console.log(data.results[i].films)
-        generateChar(data.results[i].name)
-        // char.append(printChar(data.results[i].name, data.results[i].films,
-        //   fetch(data.results[i].films)
-        //     .then((response) => response.json())
-        //     .then((data) => {console.log(data) 
-        //       char.append(printChar(data.title))
-        //     }))
-        // )
+        generateChar(data.results[i].name, data.results[i].films)
       }
     })
     .catch(err =>{
@@ -27,21 +20,23 @@ for(let i = 1; i <= 9; i++){
   }
 }
 
-function generateChar(name){
+function generateChar(name, films){
+  let container = document.createElement("div")
+  container.classList.add("container")
   let charHeader = document.createElement("h2")
   charHeader.innerText = name
-  appRoot.append(charHeader)
-}
+  container.append(charHeader)
 
-// function printChar(name, films, title){
-//   let charContainer = document.createElement("div")
-//   charContainer.classList.add("char-container")
-//   let charName = document.createElement("div")
-//   let charFilms = document.createElement("div")
-//   let filmsTitle = document.createElement("div")
-//   filmsTitle.innerText = title
-//   charFilms.innerText = films
-//   charName.innerText = name
-//   charContainer.append(charName, charFilms, filmsTitle)
-//   return charContainer
-// }
+  for(let i = 0; i < films.length; i++){
+    fetch(films[i])
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      let charFilms = document.createElement("p")
+      charFilms.innerText = data.title
+      container.append(charFilms)
+    }
+  )}
+
+  appRoot.append(container)
+}
