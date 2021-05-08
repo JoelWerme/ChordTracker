1c2464599425aae54a92148c4c83e1bb89f8da74
Joel Werme <joelwer3@gmail.com>
2021-05-08 14:38:33 +0200

    added function fetchFilms

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ for(let i = 1; i <= 9; i++){
     .then((data) => {
       console.log(data)
       for(let i = 0; i < data.results.length; i++){
-        console.log(data.results[i].name)
-        console.log(data.results[i].films)
-        generateChar(data.results[i].name, data.results[i].films)
+        generateChar(
+          data.results[i].name, 
+          data.results[i].films,
+          data.results[i].homeworld
+          )
       }
     })
     .catch(err =>{
@@ -20,23 +22,45 @@ for(let i = 1; i <= 9; i++){
   }
 }
 
-function generateChar(name, films){
+function generateChar(name, films, homeworld){
+  //create container
   let container = document.createElement("div")
   container.classList.add("container")
+  //create header with name
   let charHeader = document.createElement("h2")
   charHeader.innerText = name
+  let filmsHeader = document.createElement("h3")
+  filmsHeader.innerText = "Appearances: "
+  //add content to container
   container.append(charHeader)
+  container.append(fetchHome(homeworld))
+  container.append(filmsHeader)
+  container.append(fetchFilms(films))
+  //add container to appRoot
+  appRoot.append(container)
+}
 
+function fetchFilms(films){
+  let filmsContainer = document.createElement("div")
   for(let i = 0; i < films.length; i++){
     fetch(films[i])
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
       let charFilms = document.createElement("p")
       charFilms.innerText = data.title
-      container.append(charFilms)
+      filmsContainer.append(charFilms)
     }
   )}
+  return filmsContainer
+}
 
-  appRoot.append(container)
+function fetchHome(input){
+let charHome = document.createElement("p")  
+fetch(input)
+  .then((response) => response.json())
+  .then((data) => {
+    charHome.innerText = "Homeworld: " + data.name
+    console.log(charHome)
+  })
+  return charHome
 }
